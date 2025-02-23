0231dd699fcc1fe123d4b9d8de0fb6e027db2374
Babahasko <zakutni4ek@gmail.com>
2025-02-24 00:11:34 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request){
+	fmt.Println("Hello")
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	fmt.Println("Server is listenong on port 8081")
+	http.ListenAndServe(":8081", nil)
+}
